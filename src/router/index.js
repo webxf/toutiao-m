29b571dba7d672deb6886636d64736645117732e
webxf <j17852533489@163.com>
2022-07-15 17:54:03 +0800

    完成页面路游配置

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,30 @@ const routes = [
     path: '/login',
     // 路游懒加载
     component: () => import('@/views/login')
+  },
+  {
+    path: '/',
+    // 路游懒加载
+    component: () => import('@/views/Layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: '/video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: '/qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: '/profile',
+        component: () => import('@/views/My')
+      }
+    ]
   }
 ]
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <!-- 底部导航栏 -->
+    <van-tabbar route>
+      <van-tabbar-item to="/ ">
+        <template #icon>
+          <i class="iconfont icon-shouye"></i>
+          <span>首页</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <i class="iconfont icon-shipin"></i>
+          <span>视频</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <i class="iconfont icon-wenda"></i>
+          <span>问答</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="profile">
+        <template #icon>
+          <i class="iconfont icon-wode"></i>
+          <span>我的</span>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+:deep(.van-tabbar-item__icon) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+  .iconfont {
+    font-size: 0.53333rem;
+  }
+  span {
+    font-size: 0.32rem;
+  }
+}
+</style>
